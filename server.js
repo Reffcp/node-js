78fcbe8abdcf3e4fea3b6ab123f6c95c2a04e853
reffcp <cristian.covarrubias@rebits.com.mx>
2020-08-17 19:44:49 -0500

    avance curso peticiones

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express= require('express');
+const bodyParser= require('body-parser');
 const router=express.Router();
-var app= express();
 
+var app= express();
+app.use(bodyParser.json())
 app.use(router)
 
+
 router.get('/',(req,res) => {
+    console.log(req.headers);
+    res.send('Hola desde get');
+})
+router.post('/',(req,res) => {
+    res.send('Hola desde get');
+})
+router.delete('/',(req,res) => {
+    res.send('Hola desde get');
+})
+router.put('/',(req,res) => {
+    res.send('Hola desde get');
+})
+router.patch('/',(req,res) => {
     res.send('Hola desde get');
 })
 // app.use('/',(req,res)=> {
